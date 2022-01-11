ca92cf5ea941a403be0b225f7f442b0280d1485e
MaxM2021 <origami.freak11@gmail.com>
2022-01-11 17:24:56 -0600

    Rainbow and Eraser Buttons

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 // and 16 in horizontal layout, (visible). the etchasketching will happen with a hover effect,
 // no coordinates necessary.
 //
+//Changing the number of pixels should not grow the working area but instead shrink the pixels.
+//random color option
 
 
 function createDivs(rows, columns) {
     for (i = 0; i < rows; i++) {
          let rowContainers = document.createElement('div');
         rowContainers.classList.add('rowcontainers');
+        rowContainers.setAttribute('draggable', 'false');
         content.appendChild(rowContainers);
         
     };
@@ -16,15 +19,50 @@ function createDivs(rows, columns) {
         for (i = 0; i < columns; i++) {
             let pixel = document.createElement('div');
             pixel.classList.add('pixel');
+            pixel.setAttribute('draggable', 'false');
             row.appendChild(pixel);
         };
     });
     //Assigning the variables again for deletion and recreation
     pixels = document.querySelectorAll('.pixel');
     rowDivs = document.querySelectorAll('.rowcontainers');
+    //Listeners for the actual drawing
     pixels.forEach((pixel) => {
-         pixel.addEventListener('mouseover', () => pixel.classList.add('filled'));
+        //make drawing click and drag
+         pixel.addEventListener('mouseover', (event) => {
+            if (event.buttons == 1) {
+                if (eraserToggle == true) {
+                    pixel.style.backgroundColor = '#FFFFFF';
+                    pixel.classList.remove('filled');
+                } else if (rainbowToggle == true) {
+                    pixel.style.backgroundColor = randomColor();
+                    pixel.classList.add('filled');
+                } else {
+                    pixel.style.backgroundColor = 'red';
+                    pixel.classList.add('filled');
+                };
+            }
+            console.log(event);
+         });
+        //make one pixel drawable
+        pixel.addEventListener('click', () => {
+            if (eraserToggle == true) {
+                pixel.style.backgroundColor = '#FFFFFF';
+                pixel.classList.remove('filled');
+            } else if (rainbowToggle == true) {
+                pixel.style.backgroundColor = randomColor();
+                pixel.classList.add('filled');
+            } else {
+                pixel.style.backgroundColor = 'red';
+                pixel.classList.add('filled');
+            };
+        });
 });
+};
+
+function randomColor() {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0,6);
 };
 
 //elements
@@ -34,6 +72,10 @@ let rowDivs = document.querySelectorAll('.rowcontainers');
 
 //Buttons and Input
 const reset = document.querySelector('#reset');
+const rainbowButton = document.querySelector('#rainbow');
+const eraserButton = document.querySelector('#eraser');
+let   rainbowToggle = false;
+let   eraserToggle = false;
 const regenerate = document.querySelector('#regenerate');
 const rows = document.querySelector('#rows');
 const columns = document.querySelector('#columns');
@@ -42,6 +84,16 @@ reset.addEventListener('click', () => {
     pixels.forEach((pixel) => {
         pixel.classList.remove('filled');
     });
+    rainbowToggle = false;
+});
+
+rainbowButton.addEventListener('click', () => {
+    rainbowToggle = !rainbowToggle;
+    rainbowButton.classList.toggle('pressed');
+});
+eraserButton.addEventListener('click', () =>  {
+    eraserToggle = !eraserToggle;
+    eraserButton.classList.toggle('pressed');
 });
 
 regenerate.addEventListener('click', () => {
